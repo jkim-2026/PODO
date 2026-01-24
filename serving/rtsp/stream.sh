6d753f9e203ef3bf42449c3ba57efef92dc0501c
homsis <eop.l.8xil@gmail.com>
2026-01-24 21:12:02 +0900

    [BE] feat: stream.sh

---
+++ serving/rtsp/stream.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 1. 기존에 돌아가던 스트리머가 있다면 삭제
+sudo pm2 delete pcb-streamer || true
+
+# 2. RTSP 송출 시작
+sudo pm2 start "ffmpeg -re -stream_loop -1 -i /home/ubuntu/rtsp/PCB_Conveyor_30fps.mp4 -c copy -f rtsp rtsp://localhost:8554/pcb_stream" --name pcb-streamer --cwd /home/ubuntu/rtsp
+
+# 3. 현재 pm2 프로세스 리스트 상태 확인
+sudo pm2 status
+
+# 4. 서버가 재부팅되어도 이 설정이 유지되도록 저장
+sudo pm2 save
+
+echo "🪽 RTSP 스트리밍 설정 및 저장이 완료되었습니다."
